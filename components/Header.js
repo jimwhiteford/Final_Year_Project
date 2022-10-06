c43ddc6684c8275c9357988a0fa0f77e57cbcac7
Jim Whiteford <jim.whiteford@hotmail.com>
2022-10-06 16:12:48 +0100

    completed most of the header bar with drop down menu for mobile

--- components/Header.js
+++ components/Header.js
@@ -1,100 +1,145 @@
-import React, { useState } from "react";
-import {
-  HomeIcon,
-  SearchIcon,
-  LoginIcon,
-  CodeIcon,
-  ChevronDownIcon,
-} from "@heroicons/react/solid";
+import React, { useState, Fragment } from "react";
+import { LoginIcon } from "@heroicons/react/solid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faMicrochip } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function Header(setDrawerOpen) {
   const [dropdownOpen, setdropdownOpen] = useState(false);
   const [state, setState] = React.useState(false);
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-md">
-      <a className="flex items-center ml-3" href="/">
-        <FontAwesomeIcon
-          icon={faMicrochip}
-          className=" fa-2x text-purple-900"
-        />
-      </a>
-      <a href="/" className="ml-10 flex items-center md:hover:cursor-pointer">
-        <HomeIcon className="h-5 w-5" />
-        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
-      </a>
-      <ChevronDownIcon
-        className="flex items-center ml-2 w-5 mr-5 hover:cursor-pointer md:hidden"
-        onClick={() => setState(!state)}
-      />
       <a
-        href="/repositorys"
-        className="hidden items-center ml-10 mr-10 hover:cursor-pointer md:flex "
+        className="flex items-center ml-3 text-yellow-300 font-extrabold text-5xl"
+        href="/"
       >
-        <CodeIcon className="h-5 w-5" />
-        <p className="ml-2 hidden flex-1 lg:inline">Projects</p>
+        BUZZ
       </a>
+      <div className="flex absolute right-6 space-x-4 text-xl">
+        <div className="hidden cursor-pointer items-center p-2 md:flex">
+          <a href="/" className="text-black hover:text-yellow-400">
+            Home
+          </a>
+        </div>
+        <div className="hidden cursor-pointer items-center p-2 md:flex">
+          <a className="text-black hover:text-yellow-400">QR Scan</a>
+        </div>
+        <div className="hidden cursor-pointer items-center p-2 md:flex">
+          <a className="text-black hover:text-yellow-400">Apiarys</a>
+        </div>
+        <div className="hidden cursor-pointer items-center p-2 md:flex">
+          <a className="text-black hover:text-yellow-400">Hives</a>
+        </div>
 
-      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-400 bg-gray-100 px-3 py-1">
-        <SearchIcon
-          className="h-6 w-6 text-gray-400 mr-3"
-          onClick={() => setState(!state)}
-        />
-        <input
-          className="flex-1 bg-transparent outline-none"
-          type="text"
-          placeholder="Search"
-        ></input>
-        <button type="submit" hidden></button>
-      </form>
+        <div className="hidden cursor-pointer text-gray-400 hover:text-yellow-400 items-center p-2  md:flex">
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <LoginIcon />
+          </div>
+          <p>Sign In</p>
+        </div>
+      </div>
 
-      <div className="hidden mx-10 items-center space-x-6 lg:inline-flex "></div>
-      <div className="ml-5 flex items-center lg:hidden">
-        <FontAwesomeIcon
-          icon={faBars}
-          onClick={() => setdropdownOpen(!dropdownOpen)}
-          className="hover:cursor-pointer"
-        />
-        <div
-          className={`${
-            dropdownOpen
-              ? `top-full opacity-100 visible`
-              : "top-[110%] invisible opacity-0"
-          } absolute top-0 right-0 mt-1 mr-1 rounded border-[.5px] border-light bg-white py-5 shadow-lg transition-all`}
+      <Menu
+        as="div"
+        className="absolute right-6 text-left items-center md:hidden"
+      >
+        <div>
+          <Menu.Button className="w-full justify-center bg-white px-4 py-2">
+            <FontAwesomeIcon
+              icon={faBars}
+              onClick={() => setdropdownOpen(!dropdownOpen)}
+              className="hover:cursor-pointer text-black text-3xl items-center"
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
         >
-          <a
-            href="https://www.linkedin.com/in/jimwhiteford/"
-            target="_blank"
-            className="block py-2 px-5 text-base hover:bg-slate-500 hover:text-white"
-            onClick={() => setdropdownOpen(!dropdownOpen)}
-          >
-            LinkedIn
-          </a>
-          <a
-            href="https://github.com/jimwhiteford"
-            target="_blank"
-            className="block py-2 px-5 text-base hover:bg-slate-500 hover:text-white"
-            onClick={() => setdropdownOpen(!dropdownOpen)}
-          >
-            GitHub
-          </a>
-          <a
-            href="/"
-            className="block py-2 px-5 text-base font-semibold hover:bg-slate-500 hover:text-white"
-            onClick={() => setdropdownOpen(!dropdownOpen)}
-          >
-            Sign In
-          </a>
-        </div>
-      </div>
-
-      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <LoginIcon className="text-gray-400" />
-        </div>
-        <p className="text-gray-400">Sign In</p>
-      </div>
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-xl hover:text-yellow-400"
+                    )}
+                  >
+                    Home
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-xl hover:text-yellow-400"
+                    )}
+                  >
+                    QR Scan
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-xl hover:text-yellow-400"
+                    )}
+                  >
+                    Apiarys
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-xl hover:text-yellow-400"
+                    )}
+                  >
+                    Hives
+                  </a>
+                )}
+              </Menu.Item>
+              <form method="POST" action="#">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="submit"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block w-full px-4 py-2 text-left text-xl hover:text-yellow-400"
+                      )}
+                    >
+                      Sign out
+                    </button>
+                  )}
+                </Menu.Item>
+              </form>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </div>
   );
 }
