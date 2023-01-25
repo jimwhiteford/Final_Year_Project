1950e995a61aa19c905b14b0f14949c95be53753
Jim <jim.whiteford@hotmail.com>
2023-01-25 17:06:09 +0000

    fixed warning

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import axios from "axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
